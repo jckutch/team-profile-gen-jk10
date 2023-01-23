19daa7404117403c80bce6cd02b0b512561de3df
John Kutch <116672999+jckutch@users.noreply.github.com>
2023-01-23 13:45:09 -0500

    starting question array

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
-//Varaiable//
+// Global Varaiable
 
 const Manager = require()
 const Engineer = require()
 const Intern = require()
 const inquirer = require()
 const fs = require()
+
+// Array of questions for user input
+// Manager Input
+function manager() {
+    inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What does the user need to install to run the project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What will the project be used for?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How does the user contribute to the project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What should the user know about tests for the project?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['Apache', 'GPL', 'MIT', 'Mozilla', 'None']
+    }
+    ]
+    )};
